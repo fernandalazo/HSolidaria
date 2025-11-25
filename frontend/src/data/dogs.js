6e5549efeb941da5d283e102df947a45ccc9a915
Fernanda Lazo <fe.lazo@duocuc.cl>
2025-11-25 12:57:31 -0300

    añado lista de 12 productos categoría Perro con imágenes locales

---
+++ frontend/src/data/dogs.js
@@ -0,0 +1,91 @@
+
+// Lista de productos categoría "Perro" por categoría
+
+const dogs = [
+  {
+    id: 501,
+    titulo: "Agility Adulto Lata Cerdo 340g",
+    imagenSrc: "/img/dog_01.webp",
+    categoria: "Perro",
+    precioNuevo: 1890,
+  },
+  {
+    id: 502,
+    titulo: "Hill's Science Diet Small Paws Puppy 1.5kg",
+    imagenSrc: "/img/dog_02.webp",
+    categoria: "Perro",
+    precioNuevo: 18990,
+  },
+  {
+    id: 503,
+    titulo: "Taste of the Wild Pacific Stream 12kg",
+    imagenSrc: "/img/dog_03.webp",
+    categoria: "Perro",
+    precioNuevo: 56990,
+  },
+  {
+    id: 504,
+    titulo: "Belcando Finest GF Senior 4kg",
+    imagenSrc: "/img/dog_04.webp",
+    categoria: "Perro",
+    precioNuevo: 29990,
+  },
+  {
+    id: 505,
+    titulo: "Nutrique Toy & Mini Young Adult 2kg",
+    imagenSrc: "/img/dog_05.webp",
+    categoria: "Perro",
+    precioNuevo: 17490,
+  },
+  {
+    id: 506,
+    titulo: "Nutrique Skin Sensitivity 2kg",
+    imagenSrc: "/img/dog_06.webp",
+    categoria: "Perro",
+    precioNuevo: 18990,
+  },
+  {
+    id: 507,
+    titulo: "N&D Tropical Mini Adulto Salmón 2.5kg",
+    imagenSrc: "/img/dog_07.webp",
+    categoria: "Perro",
+    precioNuevo: 25990,
+  },
+  {
+    id: 508,
+    titulo: "Agility Adulto Lata Pollo 340g",
+    imagenSrc: "/img/dog_08.webp",
+    categoria: "Perro",
+    precioNuevo: 1890,
+  },
+  {
+    id: 509,
+    titulo: "Vitalcan Complete Senior 20kg",
+    imagenSrc: "/img/dog_09.webp",
+    categoria: "Perro",
+    precioNuevo: 34990,
+  },
+  {
+    id: 510,
+    titulo: "Belcando Senior GF 12.5kg",
+    imagenSrc: "/img/dog_10.webp",
+    categoria: "Perro",
+    precioNuevo: 69900,
+  },
+  {
+    id: 511,
+    titulo: "EntrePerros Pollo (Cubos Congelados)",
+    imagenSrc: "/img/dog_11.webp",
+    categoria: "Perro",
+    precioNuevo: 7990,
+  },
+  {
+    id: 512,
+    titulo: "Powerdog Senior Light 12kg",
+    imagenSrc: "/img/dog_12.webp",
+    categoria: "Perro",
+    precioNuevo: 34990,
+  }
+]
+
+export default dogs
