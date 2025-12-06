1dedbcfe1c6fd8129171380959d187b3993a2308
Fernanda Lazo <fe.lazo@duocuc.cl>
2025-12-05 21:13:11 -0300

    chore: elimino backend accidental, repo queda solo para frontend

--- backend/src/main/java/com/example/hsolidaria/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hsolidaria.repository;
-
-import com.example.hsolidaria.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
